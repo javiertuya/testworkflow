7b05ae1124e672a886aaf9cfd21b5f5191ecef0d
Javier Tuya <tuya@uniovi.es>
2022-01-13 19:10:04 +0100

    initial

---
+++ src/test/java/giis/samples/selema/junit4/TestSampleJunit4.java
@@ -0,0 +1,22 @@
+package giis.samples.selema.junit4;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Selenium Test Lifecycle Manager (selema) sample on the JUnit 5 framework, 
+ * see usage at https://github.com/javiertuya/selema#readme
+ */
+public class TestSampleJunit4 {
+
+	@Test
+	public void testFailMethod() {
+		assertEquals("xxx", "xxx");
+	}
+	@Test
+	public void testPassMethod() {
+		assertEquals("xxx", "xxx");
+	}
+	
+	
+}
